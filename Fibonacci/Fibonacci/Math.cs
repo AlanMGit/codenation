8ae6f2ef05f60d8487974e9173413bba4d27a912
Alan Martins <alan.m21@hotmail.com>
2020-05-18 16:22:45 -0300

    Module 1: Challenge Fibonacci

---
+++ Fibonacci/Fibonacci/Math.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Fibonacci
+{
+    class Math
+    {
+        public List<int> Fibonacci()
+        {
+            List<int> numbers = new List<int>() { 0, 1 };
+
+            while (true)
+            {
+                int total = numbers[numbers.Count()  - 2] + numbers[numbers.Count() - 1];
+
+                if (total > 350)
+                    break;
+
+                numbers.Add(total);
+            }
+
+            return numbers;
+        }
+
+        public bool IsFibonacci(int numberToTest)
+        {
+            return Fibonacci().Any(x => x == numberToTest);
+        }
+    }
+}
---
+++ Fibonacci/Fibonacci/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Fibonacci
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Desafio Fibonacci");
+
+            Math math = new Math();
+            if(math.IsFibonacci(2))
+                Console.WriteLine("Valor existe na lista");
+
+            if (!math.IsFibonacci(400))
+                Console.WriteLine("Não existe na lista");
+        }
+    }
+}
