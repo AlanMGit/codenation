248ee2ee0de80db867082c8f49d0c292df9b2708
Alan Martins <alan.m21@hotmail.com>
2020-05-24 16:21:53 -0300

    Module 2: The 10 largest Brazilian states in territorial extension

---
+++ BrazilianStates/BrazilianStates/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BrazilianStates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var country = new Country();
+            var top = country.Top10StatesByArea();
+            if (top.Length == 10)
+                Console.WriteLine("Should_Return_10_Itens_When_Get_Top_10_States");
+
+            List<State> states = country.GetStates();
+            if (states.Count == 27)
+                Console.WriteLine("Should_Return_10_Itens_When_Get_Top_10_States");
+
+            bool hasMatch = states.Any(x => top.Any(y => y == x));
+            if (!hasMatch)
+                Console.WriteLine("Should_Return_10_Itens_When_Get_Top_10_States");
+
+            
+            State lastStates = country.GetStates().OrderByDescending(x => x.Extensive).Take(1).FirstOrDefault();
+            if (top.FirstOrDefault().Extensive == lastStates.Extensive)
+                Console.WriteLine("Should_Return_Top_1_States");
+            
+        }
+    }
+}
---
+++ BrazilianStates/BrazilianStates/State.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BrazilianStates
+{
+    public class State
+    {
+        public State()
+        {
+        }
+
+        public State(string name, string acronym)
+        {
+            this.Name = name;
+            this.Acronym = acronym;
+        }
+
+        public State(string name, string acronym, double extensive)
+        {
+            this.Name = name;
+            this.Acronym = acronym;
+            this.Extensive = extensive;
+        }
+
+        public string Name { get; set; }
+
+        public string Acronym { get; set; }
+
+        public double Extensive { get; set; }
+
+    }
+}
